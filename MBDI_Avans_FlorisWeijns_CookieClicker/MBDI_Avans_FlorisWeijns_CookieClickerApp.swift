45f4444d63332b975ce9fb257fa41eb9cc5119d1
Sirith <weynsfloris@gmail.com>
2023-03-10 15:34:04 +0100

    Initial Commit

---
+++ MBDI_Avans_FlorisWeijns_CookieClicker/MBDI_Avans_FlorisWeijns_CookieClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBDI_Avans_FlorisWeijns_CookieClickerApp.swift
+//  MBDI_Avans_FlorisWeijns_CookieClicker
+//
+//  Created by Floris Weijns on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MBDI_Avans_FlorisWeijns_CookieClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
